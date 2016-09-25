7127023ebb647c2a4f96da5e1dcdf406f14cc717
Stephen John Ford <stephenjohnford@gmail.com>
2016-09-25 14:49:06 +0000

    grr

--- rcam_remote_slave/rcam_remote_slave.c
+++ rcam_remote_slave/rcam_remote_slave.c
@@ -241,10 +241,10 @@ int main(int argc, char *argv[])
 	  read(socket_fd, &previewFramerate, sizeof(previewFramerate));
 	  printf("framerate = %d\n", previewFramerate);
 	  //disable component and buffers
-	  ilclient_change_component_state(camera, OMX_StateIdle);
+	  ilclient_change_component_state(camera, OMX_StatePaused);
 	  ilclient_disable_port(camera, 70);
 	  ilclient_disable_port_buffers(camera, 70, NULL, NULL, NULL);
-	  printf("disabled");
+	  printf("disabled\n");
 	  //change the preview port
 	  setPreviewRes(camera, previewWidth, previewHeight, previewFramerate);
 	  printf("after set\n");
