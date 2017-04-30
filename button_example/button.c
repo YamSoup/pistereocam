7ad48f90dcf75595a1792fbbfc9c44244faa7613
Stephen John Ford <stephenjohnford@gmail.com>
2017-04-30 21:17:58 +0000

    changed button example completly after realizing what wiring pi was doing with ISR

--- button_example/button.c
+++ button_example/button.c
@@ -13,33 +13,36 @@ int count = 0;
 uint16_t state = 0;
 pthread_mutex_t interupt_mutex;
 
-void myInterrupt()
+void *myButtonPoll()
 {
-  //NEED to check if mutex is locked and abort
-  //possibly change to pthread mutex so i can do:
+  //needs a lot of work! but getting there
+  int x;
+  while(1) {
   if (pthread_mutex_trylock(&interupt_mutex) == 0) {
     //this chunk of code is working quite well
     state = 0;
-    int x;
-    for(x = 0; x < 500; x++) {
+    for(x = 0; x < 20; x++) {
+      //printf("state = ox%x\n", state);
       state = state | (uint16_t)digitalRead(PIN_NUM);
-      if (state == 0xffff) {
+      if (state == 0x0000) {
+	//might need to test for button up with 0xffff
 	printf("Debounced Button Press\n");
+	sleep(1);
 	break;
       }
       state = state << 1;    
     }
-    pthread_mutex_unlock(&interupt_mutex);
-    return;
+  pthread_mutex_unlock(&interupt_mutex);
   }
-  printf("trylock block\n");
-  return;
-  
+  else printf("trylock block\n");  
+  }
 }
 
 int main (void)
 {
   int result = 0;
+  pthread_t button_id;
+  
   result = pthread_mutex_init(&interupt_mutex, NULL);
   printf("Mutex initalised state = %d\n", result);
   result = wiringPiSetup();
@@ -47,9 +50,16 @@ int main (void)
   
   pinMode(PIN_NUM, INPUT);
   pullUpDnControl(PIN_NUM, PUD_UP);
+
+  // DO NOT USE
+  // wiringPiISR(PIN_NUM, INT_EDGE_BOTH, &myInterrupt); 
+  // the 'ISR' WiringPi solution boils down to a high prioty pthread with a for loop
+  // leaving this warning in as a reminder not to use
+
+  result = pthread_create(&button_id, NULL, myButtonPoll, NULL);
+  printf("thread result = %d\n", result);
   
-  wiringPiISR(PIN_NUM, INT_EDGE_FALLING, &myInterrupt); 
-  printf("pre loop");
+  printf("pre main loop");
   while(1)
     {
       usleep(400);
